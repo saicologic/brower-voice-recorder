29dedf9691bd7c884a421f5c6a16bf776781c618
satoru.mikami <satoru.mikami@voicy.jp>
2020-02-11 21:36:28 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+      "es6": true,
+      "node": true
+    },
+    "extends": "eslint:recommended",
+    "plugins": ["node"],
+    "rules": {
+        "semi": "error"
+    }
+};

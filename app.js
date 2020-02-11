343a3e6cfef806c6b628f5f1b6dbc1b9cab03b7c
satoru.mikami <satoru.mikami@voicy.jp>
2020-02-11 21:36:36 +0900

    セミコロンを追加

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const fs = require('fs')
-const WavEncoder = require('wav-encoder')
+const fs = require('fs');
+const WavEncoder = require('wav-encoder');
 const port = 3000;
 const sampleRate = 48000;
-const filename = 'output/sample.wav'
+const filename = 'output/sample.wav';
 
 let buffer = [];
 
@@ -19,7 +19,7 @@ const toArrayBuffer = function(buf) {
         view[i] = buf[i];
     }
     return ab;
-}
+};
 
 io.on('connection', function (socket) {
     socket.on('join', function () {
@@ -27,12 +27,12 @@ io.on('connection', function (socket) {
     });
 
     socket.on('audioData', function (message) {
-        let values = message.values()
-        let buf = new Array(message.length)
+        let values = message.values();
+        let buf = new Array(message.length);
         for (var i = 0; i < buf.length; i++) {
-            buf[i] = values.next().value
+            buf[i] = values.next().value;
         }
-        buffer = buffer.concat(buf)
+        buffer = buffer.concat(buf);
     });
 
     socket.on('stop', () => {
@@ -47,12 +47,12 @@ io.on('connection', function (socket) {
         WavEncoder.encode(audioData).then((buffer) => {
             fs.writeFile(filename, Buffer.from(buffer), (e) => {
                 if (e) {
-                    console.log(e)
+                    console.log(e);
                 } else {
-                    console.log(`Output ${filename}`)
+                    console.log(`Output ${filename}`);
                 }
-            })
-        })
+            });
+        });
     });
 });
 
